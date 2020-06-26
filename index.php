fbff47f9f20cd3745316c6e2af18b660b4bb437a
vukolovanton <vukolovanton92@gmail.com>
2020-06-26 15:16:00 +0400

    Navigation panel styling

--- index.php
+++ index.php
@@ -15,70 +15,97 @@ include("includes/config.php");
 </head>
 
 <body>
-    <div id="nowPlayingBarContainer">
-        <div id="nowPlayingBar">
-            
-            <div id="nowPlayingLeft">
-                <div class="content">
-                    <span class="albumLink">
-                        <img class="albumArtwork" src="https://ae01.alicdn.com/kf/HTB1zZKTfxTI8KJjSspiq6zM4FXa2/Oct-Home-Textile-Azur-Lane-USS-Vestal-Anime-Girl-One-sided-Two-sided-Polyester-Square-Pillow.jpg" >
-                    </span>
-                    <div class="trackInfo">
-                        <span class="trackName">
-                            <span>Gay song</span>
+    <div id="mainContainer">
+        <div id="topContainer">
+            <div id="navBarContainer">
+                <nav class="navBar">
+                    <a class="logo" href="index.php">Spotify</a>
+
+                    <div class="group">
+                        <div class="navItem">
+                            <a href="search.php" class="navItemLink">Search</a>
+                        </div>
+                    </div>
+
+                    <div class="group">
+                        <div class="navItem">
+                        <a href="browse.php" class="navItemLink">Browse</a>
+                        </div>
+                        <div class="navItem">
+                        <a href="yourMusic.php" class="navItemLink">Your Music</a>
+                        </div>
+                        <div class="navItem">
+                        <a href="profile.php" class="navItemLink">Profile</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <div id="nowPlayingBarContainer">
+            <div id="nowPlayingBar">
+                
+                <div id="nowPlayingLeft">
+                    <div class="content">
+                        <span class="albumLink">
+                            <img class="albumArtwork" src="https://ae01.alicdn.com/kf/HTB1zZKTfxTI8KJjSspiq6zM4FXa2/Oct-Home-Textile-Azur-Lane-USS-Vestal-Anime-Girl-One-sided-Two-sided-Polyester-Square-Pillow.jpg" >
                         </span>
-                        <span class="artistName">
-                            <span>AnimeLover69</span>
-                        </span>
+                        <div class="trackInfo">
+                            <span class="trackName">
+                                <span>Gay song</span>
+                            </span>
+                            <span class="artistName">
+                                <span>AnimeLover69</span>
+                            </span>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div id="nowPlayingCenter">
-                <div class="content playerControls">
-                    <div class="buttons">
-                        <button class="controlButton shuffle" title="Suffle button">
-                            <img src="assets/svg/027-shuffle arrows.svg" alt="Shuffle">
-                        </button>
+                <div id="nowPlayingCenter">
+                    <div class="content playerControls">
+                        <div class="buttons">
+                            <button class="controlButton shuffle" title="Suffle button">
+                                <img src="assets/svg/027-shuffle arrows.svg" alt="Shuffle">
+                            </button>
 
-                        <button class="controlButton previous" title="Previous button">
-                            <img src="assets/svg/024-previous.svg" alt="Previous">
-                        </button>
+                            <button class="controlButton previous" title="Previous button">
+                                <img src="assets/svg/024-previous.svg" alt="Previous">
+                            </button>
 
-                        <button class="controlButton play" title="Play button">
-                            <img src="assets/svg/020-play.svg" alt="Play">
-                        </button>
+                            <button class="controlButton play" title="Play button">
+                                <img src="assets/svg/020-play.svg" alt="Play">
+                            </button>
 
-                        <button class="controlButton pause" title="Pause button" style="display: none;">
-                            <img src="assets/svg/021-pause.svg" alt="Pause">
-                        </button>
+                            <button class="controlButton pause" title="Pause button" style="display: none;">
+                                <img src="assets/svg/021-pause.svg" alt="Pause">
+                            </button>
 
-                        <button class="controlButton next" title="Next button">
-                            <img src="assets/svg/023-next.svg" alt="Next">
-                        </button>
+                            <button class="controlButton next" title="Next button">
+                                <img src="assets/svg/023-next.svg" alt="Next">
+                            </button>
 
-                        <button class="controlButton repeat" title="Repeat button">
-                            <img src="assets/svg/028-repeat.svg" alt="Repeat">
-                        </button>
-                    </div>
-                    <div class="playbackBar">
-                        <span class="progressTime current">
-                            0.00
-                        </span>
-                        <div class="progressBar">
-                            <div class="progressBarBg">
-                                <div class="progress">
+                            <button class="controlButton repeat" title="Repeat button">
+                                <img src="assets/svg/028-repeat.svg" alt="Repeat">
+                            </button>
+                        </div>
+                        <div class="playbackBar">
+                            <span class="progressTime current">
+                                0.00
+                            </span>
+                            <div class="progressBar">
+                                <div class="progressBarBg">
+                                    <div class="progress">
+                                    </div>
                                 </div>
                             </div>
+                            <span class="progressTime remaining">
+                                0.00
+                            </span>
                         </div>
-                        <span class="progressTime remaining">
-                            0.00
-                        </span>
                     </div>
                 </div>
-            </div>
 
-            <div id="nowPlayingStart"></div>
+                <div id="nowPlayingStart"></div>
+            </div>
         </div>
     </div>
 </body>
